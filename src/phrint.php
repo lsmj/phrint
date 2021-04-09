80a90ea006e6b290ebe963c8bf145394612a248b
Lyder Stefan Minde Janøy <lyderjanoy@gmail.com>
2021-04-09 12:43:52 +0200

    Changed input to var and reworked the p method

--- src/phrint.php
+++ src/phrint.php
@@ -5,26 +5,38 @@ namespace lsmj;
 class phrint
 {
 
-	/**
+    /**
 	 *
 	 * p (Print): Outputs type and value of input with html formatting
 	 *
-	 * @param mixed $input
+	 * @param mixed $var
 	 *
 	 * @return void
 	 */
-	static function p($input)
+	static function p($var)
 	{
 		echo '<pre style="background-color:#dfdac4;padding:20px;border-radius:10px;color:#452317;">';
-		if (is_array($input)) {
-			echo '<strong>Type: ' . gettype($input) . '</strong><br>';
-			print_r($input);
-		} elseif (is_object($input)) {
-			echo '<strong>Type: ' . gettype($input) . '</strong><br>';
-			echo json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+		if (is_array($var)) {
+			print_r($var);
+		} elseif (is_object($var)) {
+			echo ucfirst(gettype($var)) . '<br>';
+			echo json_encode($var, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 		} else {
-			echo '<strong>Type: ' . gettype($input) . '</strong><br>';
-			echo $input;
+			if (is_bool($var) === true) {
+				echo ucfirst(gettype($var)) . '<br>';
+				if ($var) {
+					echo 'TRUE';
+				} else {
+					echo 'FALSE';
+				}
+			} else {
+				echo '<strong>' . ucfirst(gettype($var)) . '</strong><br>';
+				if (is_string($var)) {
+					echo '"' . $var . '"';
+				} else {
+					echo $var;
+				}
+			}
 		}
 		echo '</pre>';
 	}
@@ -33,21 +45,21 @@ class phrint
 	 *
 	 * C (Console): Outputs type and value of input without html formatting
 	 *
-	 * @param mixed $input
+	 * @param mixed $var
 	 *
 	 * @return void
 	 */
-	static function c($input)
+	static function c($var)
 	{
-		if (is_array($input)) {
-			echo 'Type: ' . gettype($input) . PHP_EOL;
-			print_r($input);
-		} elseif (is_object($input)) {
-			echo 'Type: ' . gettype($input) . PHP_EOL;
-			echo json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+		if (is_array($var)) {
+			echo 'Type: ' . gettype($var) . PHP_EOL;
+			print_r($var);
+		} elseif (is_object($var)) {
+			echo 'Type: ' . gettype($var) . PHP_EOL;
+			echo json_encode($var, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 		} else {
-			echo 'Type: ' . gettype($input) . PHP_EOL;
-			echo $input;
+			echo 'Type: ' . gettype($var) . PHP_EOL;
+			echo $var;
 		}
 	}
 
@@ -55,14 +67,14 @@ class phrint
 	 *
 	 * m (Message): Outputs the input in the same style as p()
 	 *
-	 * @param mixed $input
+	 * @param mixed $var
 	 *
 	 * @return void
 	 */
-	static function m($input)
+	static function m($var)
 	{
 		echo '<pre style="background-color:#dfdac4;padding:20px;border-radius:10px;color:#452317;">';
-		echo $input;
+		echo $var;
 		echo '</pre>';
 	}
 
@@ -70,21 +82,22 @@ class phrint
 	 *
 	 * l (List): Prints the input as a vertical list
 	 *
-	 * @param string $input string
+	 * @param string $var string
 	 *
 	 * @return void
 	 */
-	static function l(string $input, string $delimiter = ",", string $remove_string = null)
+	static function l(string $var, string $delimiter = ",", string $remove_string = null)
 	{
 		if ($remove_string) {
 			$remove_string = preg_quote($remove_string, "/");
-			$input = preg_replace("/$remove_string/", '', $input);
+			$var = preg_replace("/$remove_string/", '', $var);
 		}
-		$items = explode($delimiter, $input);
+		$items = explode($delimiter, $var);
 		echo '<pre style="background-color:#dfdac4;padding:20px;border-radius:10px;color:#452317;">';
 		foreach ($items as $item) {
 			echo $item . '<br>';
 		}
 		echo '</pre>';
 	}
+
 }
